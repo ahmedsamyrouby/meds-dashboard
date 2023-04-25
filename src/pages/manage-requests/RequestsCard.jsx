9e1c9f57defb8be39346aebb9750cb7fa4af813f
ahmedsamy2003 <70441494+ahmedsamy2003@users.noreply.github.com>
2023-04-25 20:30:27 +0200

    manage-requests backend integration

--- src/pages/manage-requests/RequestsCard.jsx
+++ src/pages/manage-requests/RequestsCard.jsx
@@ -1,27 +1,69 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Button, Accordion, Alert } from "react-bootstrap";
 
-const RequestCard = ({eventKey}) => {
+const RequestCard = ({ reqId, userName, catName, medName, reqStatus }) => {
+
+  const [status, setStatus] = useState(reqStatus)
+
+  const handleAccept = () => {
+
+    setStatus("accept");
+
+    axios.put("http://localhost:3001/respo/" + reqId, {
+      statut_req: "accept",
+    });
+  };
+
+  const handleDecline = () => {
+
+    setStatus("decline");
+
+    axios.put("http://localhost:3001/respo/" + reqId, {
+      statut_req: "decline",
+    });
+  };
+
+
   return (
-      <Accordion.Item eventKey={eventKey} className="requests-list-item bg-light m-3">
-        <Accordion.Header>
-          Request Item
-        </Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam.
-          <div>
-          <Button className="btn btn-success me-2 my-2">Accept</Button>
-          <Button className="btn btn-danger me-2 my-2">Decline</Button>
-          {/* <Alert className = "my-3" variant="success">
-            Request Accepted
-          </Alert>
-          <Alert className = "my-3" variant="danger">
-            Request Denied
-          </Alert> */}
-          </div>
-        </Accordion.Body>
-      </Accordion.Item>
+    <Accordion.Item
+      eventKey={reqId}
+      className="requests-list-item bg-light m-3"
+    >
+      <Accordion.Header>
+        Request<span className="fw-bold">#{reqId}</span>: {userName}
+      </Accordion.Header>
+      <Accordion.Body>
+        <p className="medicine-name">
+          <span className="fw-bold">Medicine Name: </span>
+          {medName}
+        </p>
+        <p className="medicine-category">
+          <span className="fw-bold">Medicine Category: </span>
+          {catName}
+        </p>
+        <div>
+          {!status ? (
+            <div>
+              <Button onClick={handleAccept} value="accept" className="btn btn-success me-2 my-2">
+                Accept
+              </Button>
+              <Button onClick={handleDecline} value="decline" className="btn btn-danger me-2 my-2">
+                Decline
+              </Button>
+            </div>
+          ) : status === "accept" ? (
+            <Alert className="my-3" variant="success">
+              Request Accepted
+            </Alert>
+          ) : (
+            <Alert className="my-3" variant="danger">
+              Request Denied
+            </Alert>
+          )}
+        </div>
+      </Accordion.Body>
+    </Accordion.Item>
   );
 };
 
--- src/pages/manage-requests/RequestsManager.jsx
+++ src/pages/manage-requests/RequestsManager.jsx
@@ -1,28 +1,62 @@
-import { Container, Accordion } from "react-bootstrap";
+import { Container, Accordion, Spinner } from "react-bootstrap";
 import RequestsCard from "./RequestsCard";
 import "./styles/RequestsManager.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const RequestsManager = () => {
+  const [requests, setRequests] = useState({
+    loading: true,
+    reqData: [],
+    err: null,
+    reload: 0,
+  });
+
+  useEffect(() => {
+    setRequests({ ...requests, loading: true });
+    axios
+      .get("http://localhost:3001/request")
+      .then((res) => {
+        setRequests({
+          ...requests,
+          loading: false,
+          reqData: res.data,
+          err: null,
+        });
+      })
+      .catch((e) => {
+        setRequests({ ...requests, loading: false, err: e });
+        console.log(e);
+      });
+  }, []);
+
   return (
     <Container className="request-manager-container p-4 bg-dark m-5">
+      {requests.loading && (
+        <div className="text-center">
+          <Spinner animation="border" role="status" variant="light">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      )}
       <header className="request-manager-header p-4">
         <h1>Manage Requests</h1>
       </header>
-      <Accordion>
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-        <RequestsCard />
-      </Accordion>
+      {!requests.loading && (
+        <Accordion>
+          {requests.reqData.map((req) => {
+            return(
+              <RequestsCard
+                reqId={req.idfreq}
+                userName={req.nameofuser}
+                catName={req.namenewca}
+                medName={req.namenewmeds}
+                reqStatus={req.statut_req}
+              />
+            )
+          })}
+        </Accordion>
+      )}
     </Container>
   );
 };
