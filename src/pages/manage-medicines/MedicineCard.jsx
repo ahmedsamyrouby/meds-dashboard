ad254eb8613e073d950de55248b724f1ebe3c4b1
ahmedsamy2003 <70441494+ahmedsamy2003@users.noreply.github.com>
2023-05-02 04:26:16 +0300

    manage medicines list be integration

--- src/pages/manage-medicines/MedicineCard.jsx
+++ src/pages/manage-medicines/MedicineCard.jsx
@@ -12,7 +12,7 @@ import { MdInfo } from "react-icons/md";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const MedicineCard = ({ MedId }) => {
+const MedicineCard = ({ medId, expDate, medName, medDesc, medCat, medPrice }) => {
   const CustomToggle = ({ eventKey }) => {
     const [clicked, setClicked] = useState(false);
 
@@ -36,47 +36,44 @@ const MedicineCard = ({ MedId }) => {
     <Card className="mb-3">
       <Card.Header className="d-flex align-items-center">
         <p className="d-inline my-0 me-auto fs-5 align-self-center">
-          Medicine Name
+          {medName}
         </p>
         <Button variant="danger" className=" py-2 px-2 me-3">
           <RiDeleteBin6Fill size="1.5rem" />
         </Button>
-        <Link to={""+MedId} className="btn btn-secondary py-2 px-2 me-3">
+        <Link to={""+medId} className="btn btn-secondary py-2 px-2 me-3">
           <RiEdit2Fill color="white" size="1.5rem" />
         </Link>
-        <CustomToggle className="me-3" eventKey={MedId}>
+        <CustomToggle className="me-3" eventKey={medId}>
           {/*show details button*/}
         </CustomToggle>
       </Card.Header>
-      <Accordion.Collapse eventKey={MedId}>
+      <Accordion.Collapse eventKey={medId}>
         <Card.Body>
           {/* DESCRIPTION */}
           <div className="mb-2">
             <span className="fw-bold">Medicine Description: </span>
             <p className="m-0">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              {medDesc}
             </p>
           </div>
 
           {/* EXPIRATION DATE */}
           <div className="mb-2">
             <span className="fw-bold">Expiration Date: </span>
-            <span>20/12/2015</span>
+            <span>{expDate.slice(0, 10)}</span>
           </div>
 
           {/* CATEGORY */}
           <div className="mb-2">
             <span className="fw-bold">Category: </span>
-            <Badge>#category</Badge>
+            <Badge>#{medCat}</Badge>
           </div>
 
           {/* PRICE */}
           <div className="mb-2">
             <span className="fw-bold">Price: </span>
-            <span className="fw-semibold opacity-75">EGP69</span>
+            <span className="fw-semibold opacity-75">EGP{medPrice ? medPrice : 0}</span>
           </div>
         </Card.Body>
       </Accordion.Collapse>
--- src/pages/manage-medicines/MedicinesManager.jsx
+++ src/pages/manage-medicines/MedicinesManager.jsx
@@ -1,8 +1,36 @@
 import { Accordion, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import MedicineCard from "./MedicineCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../App";
 
 const ManageMedicines = () => {
+
+  const [meds, setMeds] = useState({
+    loading: true, 
+    medsData: [],
+    err: null,
+  });
+
+  const getMeds = () => {
+    axios.get(BASE_URL + "/med").then(res =>{
+      setMeds({
+        loading: false,
+        medsData: res.data,
+        err: null
+      })
+    }).catch(e => {
+      setMeds({
+        ...meds, loading: false, err: e.msg
+      })
+    })
+  }
+
+  useEffect(()=>{
+    getMeds();
+  }, [])
+
   return (
     <Container className="rounded-4 request-manager-container p-4 bg-dark m-5 d-flex flex-column">
       <header className="m-4">
@@ -14,7 +42,21 @@ const ManageMedicines = () => {
         </Link>
       </Button>
       <Accordion className="p-3">
-        <MedicineCard MedId={1} />
+        {
+          meds.medsData.map(med => {
+            return (
+              <MedicineCard 
+              key={med.id_med}
+              expDate = {med.Expiration_date}
+              medName = {med.Name_meds}
+              medDesc = {med.description_meds}
+              medId = {med.id_med}
+              medCat = {med.namefcategory}
+              medPrice = {med.price}
+              />
+            )
+          }) 
+        }
       </Accordion>
     </Container>
   );
